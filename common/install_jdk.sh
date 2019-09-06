33debb96727c3bf5466265b6cd68507cb2ce8a37
chloneda <chen_hailin@topsec.com.cn>
2019-09-06 23:40:57 +0800

    update common

---
+++ common/install_jdk.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+BASE_SERVER=192.168.1.11
+
+yum install -y wget
+wget $BASE_SERVER/soft-all/jdk-7u45-linux-x64.tar.gz
+
+tar -zxvf jdk-7u45-linux-x64.tar.gz -C /opt/tools
+
+cat >> /etc/profile << EOF
+export JAVA_HOME=/usr/local/jdk1.7.0_45
+export PATH=\$PATH:\$JAVA_HOME/bin
+EOF
+
